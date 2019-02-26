d8fcda35bc1eb2bcbd53278f581023b79ccafeec
Sarah <sjmadro1@gmail.com>
2019-02-26 22:58:24 +0000

    query famous_people with knex

---
+++ knex_script.js
@@ -0,0 +1,25 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: settings.hostname,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database
+  }
+});
+
+  const command = process.argv[2];
+
+  knex.from('famous_people').select('*').where('first_name', command).orWhere('last_name', command)
+    .then((result) => {
+      console.log('result:', result);
+    }).catch((err) => {
+      console.log('error:', err);
+    });
+
+
+
+
